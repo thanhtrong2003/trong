446718b6ba705460e6da8b66e41dbb6de9865378
thanhtrong2003 <vothanhtrongmkt@gmail.com>
2023-09-18 13:53:51 +0700

    init project

---
+++ Vothanhtrong_2121110273/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Vothanhtrong_2121110273
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtUserName = new TextBox();
+            txtPassword = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // txtUserName
+            // 
+            txtUserName.Location = new Point(98, 75);
+            txtUserName.Name = "txtUserName";
+            txtUserName.Size = new Size(141, 23);
+            txtUserName.TabIndex = 0;
+            txtUserName.TextChanged += txtUserName_TextChanged;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(98, 138);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(141, 23);
+            txtPassword.TabIndex = 1;
+            txtPassword.TextChanged += txtPassword_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(24, 83);
+            label1.Name = "label1";
+            label1.Size = new Size(29, 15);
+            label1.TabIndex = 2;
+            label1.Text = "user";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(24, 146);
+            label2.Name = "label2";
+            label2.Size = new Size(30, 15);
+            label2.TabIndex = 3;
+            label2.Text = "pass\r\n";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(98, 194);
+            button1.Name = "button1";
+            button1.Size = new Size(76, 30);
+            button1.TabIndex = 4;
+            button1.Text = "Login\r\n";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUserName);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtUserName;
+        private TextBox txtPassword;
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private Label user;
+    }
+}
