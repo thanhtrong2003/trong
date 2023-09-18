c6e9e8b43aef10b3da9e332634a3e6c421ada50f
thanhtrong2003 <144524388+thanhtrong2003@users.noreply.github.com>
2023-09-18 13:40:50 +0700

    Add files via upload

---
+++ Vothanhtrong_2121110273/Vothanhtrong_2121110273/Form1.cs
@@ -0,0 +1,31 @@
+namespace Vothanhtrong_2121110273
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string strUser = txtUserName.Text;
+            string strPass = txtPassword.Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtUserName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
